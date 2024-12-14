16489c82128f3ce6c4219d61906a7bbec83973d9
Kunal Gajanan Adkine <kunaladkine90@gmail.com>
2024-12-14 21:43:28 +0530

    day1

---
+++ helloword.js
@@ -0,0 +1,7 @@
+console.log("hellow i am learning javascript")
+// this is the single line comment i have leanerd
+
+/* this is multi line comment
+for uses multiple line*/
+
+// We Can Print The js program by using the simple 'console.log()' function
---
+++ variable.js
@@ -0,0 +1,23 @@
+// we can declare the variable by different like let, const
+
+let firstName ="kunal"
+let lastName="Adkine"
+let country='India'
+let age=23
+
+const number=234
+const aadhar=5639229
+
+job='It'
+date=26
+
+
+console.log(firstName,lastName,country,age)
+console.log(firstName)
+console.log(lastName)
+console.log(job)
+
+// number=342
+// console.log(number)         //it will give error becaue const value declare then can not be changed.
+
+
